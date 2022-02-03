5d1172c5b76e98fd7486fa14de76b00a8780ad41
=mplecapt <=mplecapt@mtu.edu>
2022-02-03 15:40:43 -0500

    Started to implement tokens, have a list of edge cases

--- src/PlayerProgress.js
+++ src/PlayerProgress.js
@@ -9,12 +9,46 @@ export default class PlayerProgress extends Component {
 	}
 
 	async componentDidMount() {
+		let prog = await GetPlayerProgress(this.props.steamid);
+
+		const tests = [
+			{ regx: /.*(four man crew)|(four players)/gi, symbol: '4', value: 'Four man crew required'},
+			{ regx: /(?<= the )((?!.*(armored)|(plans)|(train)).+?)((?= job[^s])|(?= heist\W))/gi },
+			{ regx: /(?!.*job,)(?<= the ).*? escape(?=,)/gi },
+			{ regx: /armored transport/gi },
+			{ regx: /.*(floyd)|(bobblehead bob)/gi, symbol: 'Big Bank', value: 'Big Bank'},
+			{ regx: /Panic Room/gi, symbol: 'Panic Room', value: 'Panic Room'},
+		]
+		prog.achievements.forEach(a => {
+			// perform each test
+			tests.forEach(t => {
+				const matches = a.description?.match(t.regx);
+				if (matches)
+					// add each match found
+					matches.forEach(m => {
+						const sym = t.symbol ? t.symbol : m;
+						if (sym.includes("Celebrating")) return;
+						// if tokens does not alread contain the new token
+						if (a.tokens.filter(t => t.symbol === sym).length === 0)
+							// add new token
+							a.tokens.push({
+								symbol: sym,
+								value: t.value ? t.value : m
+							})
+					})
+			});
+		});
+
 		this.setState({
-			progress: await GetPlayerProgress(this.props.steamid)
+			progress: prog
 		})
 	}
 
 	render() {
+		if (!API_KEY) {
+			console.log("Error! No apikey set");
+			return <></>;
+		}
 		if (!this.props.steamid) {
 			console.log("Error! No steamid set");
 			return <></>;
@@ -25,12 +59,22 @@ export default class PlayerProgress extends Component {
 				{this.state.progress.achievements.filter(a => {
 					const filter = this.props.filterBy;
 					return (
+						// must exclude already achieved cards
 						(filter.removeComplete && !a.achieved) ||
 						!filter.removeComplete
 					) &&
 					(
+						// name or description includes exact search terms
 						contains(a.displayName, filter.search) ||
 						(!a.hidden && contains(a.description, filter.search))
+					) &&
+					(
+						// must include 4 man crew token
+						(filter.fourman === 'mustInclude' && a.tokens.some(t => t.symbol === '4')) ||
+						// must exclude 4 man crew token
+						(filter.fourman === 'exclude' && !a.tokens.some(t => t.symbol === '4')) ||
+						// ignore 4 man crew token
+						(filter.fourman !== 'mustInclude' && filter.fourman !== 'exclude')
 					)
 				}).map(a => (
 					<Card key={a.name} data={a} />
@@ -51,12 +95,21 @@ function contains(str1, str2) {
 function Card({ data }) {
 	return (
 		<div className='card'>
-			<div className='tooltip'>
-				<img className='card-img' alt={data.name} src={(data.achieved) ? data.icon : data.icongray} />
-				{data.description && <span className='tooltiptext'>{data.description}</span>}
+			<div className='data-container'>
+				<div className='tooltip'>
+					<img className='card-img' alt={data.name} src={(data.achieved) ? data.icon : data.icongray} />
+					{data.description && <span className='tooltiptext'>{data.description}</span>}
+				</div>
+				<span className='card-name'>{data.displayName}</span>
+				<span className='card-date'>{data.achieved && (new Date(data.unlocktime * 1000)).toLocaleString()}</span>
 			</div>
-			<span className='card-name'>{data.displayName}</span>
-			<span className='card-date'>{data.achieved && (new Date(data.unlocktime * 1000)).toLocaleString()}</span>
+			{data.tokens && data.tokens.length > 0 && (
+				<div className='token-container'>
+					{data.tokens.map(t => (
+						<span key={t.symbol} className='token'>{t.symbol}</span>
+					))}
+				</div>
+			)}
 		</div>
 	)
 }
@@ -95,6 +148,7 @@ async function GetPlayerProgress(userid) {
 				icongray: a.icongray,
 				achieved: player.playerstats.achievements[i].achieved === 1,
 				unlocktime: player.playerstats.achievements[i].unlocktime,
+				tokens: []
 			}
 		})
 	}
--- src/Search.js
+++ src/Search.js
@@ -4,6 +4,7 @@ import PlayerProgress from './PlayerProgress';
 const INIT_STATE = {
 	search: '',
 	removeComplete: false,
+	fourman: 'canInclude',
 }
 
 export default class SearchForm extends Component {
@@ -18,8 +19,11 @@ export default class SearchForm extends Component {
 
 	handleChange(event) {
 		const target = event.target;
-		const value = target.type === 'checkbox' ? target.checked : target.value;
+		let value = target.type === 'checkbox' && target.name !== 'fourman' ? target.checked : target.value;
 		const name = target.name;
+
+		if (name === 'fourman' && value === this.state.fourman) 
+			value = 'canInclude';
 
 		this.setState({
 			[name]: value
@@ -39,13 +43,26 @@ export default class SearchForm extends Component {
 	render() {
 		return (
 			<>
-				<form onSubmit={this.handleSubmit}>
+				<form onSubmit={this.handleSubmit} style={{color: 'white'}}>
 					<div>
 						<input type='text' name='search' value={this.state.search} onChange={this.handleChange} placeholder="Search..." />
 					</div>
 					<div>
-						<input type='checkbox' name='removeComplete' checked={this.state.removeComplete} onChange={this.handleChange} />
-						<label htmlFor="removeComplete" style={{color: 'white'}}>Incomplete Only</label>
+						<label>
+							<input type='checkbox' name='removeComplete' checked={this.state.removeComplete} onChange={this.handleChange} />
+							Incomplete Only
+						</label>
+					</div>
+					<div>
+						<label>4 Man Crews: </label>
+						<label>
+							<input type='checkbox' name='fourman' value='mustInclude' checked={this.state.fourman === 'mustInclude'} onChange={this.handleChange} />
+							Must Include
+						</label>
+						<label>
+							<input type='checkbox' name='fourman' value='exclude' checked={this.state.fourman === 'exclude'} onChange={this.handleChange} />
+							Exclude
+						</label>
 					</div>
 					<div>
 						<button type='reset' onClick={this.handleReset}>Reset</button>
